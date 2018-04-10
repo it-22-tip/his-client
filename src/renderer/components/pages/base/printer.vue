6ea266eab4e06231e701cf86f0cc8ae4b5496492
itphcccoid <35017703+idcophccit@users.noreply.github.com>
2018-04-10 13:07:44 +0700

    printer

--- src/renderer/components/pages/base/printer.vue
+++ src/renderer/components/pages/base/printer.vue
@@ -1,19 +1,87 @@
 <template>
   <div class="printer">
     <img :src="img">
+    <button @click="getSupportedJobCommands">Supported Job</button>
+    <button @click="getSupportedPrintFormats">Supported Format</button>
+    <button @click="getPrinters">List Printer</button>
+    <button @click="print">Print</button>
   </div>
 </template>
 
 <script>
+  import printer from 'printer'
+  import util from 'util'
+  import file from '@/mixins/file'
   export default {
     name: 'Intro',
+    mixins: [file],
     data () {
       return {
-        img: `static/svg/logo.svg`
+        img: `static/svg/logo.svg`,
+        printdata: null
       }
     },
     created () {
 
+    },
+    mounted () {
+      // console.log("installed printers:\n"+util.inspect(printer.getPrinters(), {colors:true, depth:10}))
+      console.log(printer)
+      this.readFile(__static + '/svg/logo.svg').then(
+        data => {
+          console.log(data)
+          this.printdata = data
+        }
+      )
+    },
+    methods: {
+      getSupportedPrintFormats () {
+        let formats
+        try {
+          formats = printer.getSupportedPrintFormats()
+        } catch (error) {
+          console.log(error)
+        }
+        console.log({formats: formats})
+      },
+      getPrinters () {
+        try {
+          this.formats = printer.getPrinters()
+        } catch (error) {
+          console.log(error)
+        }
+      },
+      getPrinter () {
+        try {
+          this.formats = printer.getPrinter()
+        } catch (error) {
+          console.log(error)
+        }
+      },
+      getSupportedJobCommands () {
+        let jobs
+        try {
+          jobs = printer.getSupportedJobCommands()
+        } catch (error) {
+          console.log(error)
+        }
+        console.log({jobs: jobs})
+      },
+      print () {
+        printer.printDirect(
+          {
+            data: this.printdata,//'print from Node.JS buffer', // or simple String: "some text"
+            printer: 'EPSON-L360-Series', //, printer:'Foxit Reader PDF Printer' // printer name, if missing then will print to default printer
+            type: 'PDF', // type: RAW, TEXT, PDF, JPEG, .. depends on platform
+            success: function (jobID) {
+              console.log("sent to printer with ID: "+jobID)
+            },
+            error: function (err) {
+              console.log(err)
+            }
+          }
+        )
+      }
     }
   }
 </script>
