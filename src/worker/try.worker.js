5f744600e5066afc741c2bd9c98562552bc0a72a
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-05-13 16:55:19 +0700

    remove worker

--- src/worker/try.worker.js
+++
@@ -1,9 +0,0 @@
-// Post data to parent thread
-let k = window.Buffer
-try {
-  self.postMessage({ foo: k })
-} catch (err) {
-  console.log(err)
-}
-// Respond to message from parent thread
-self.addEventListener('message', (event) => console.log(event))
