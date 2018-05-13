b9c118b851cfb1ff9a517d2811939029e0463785
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-05-13 18:33:25 +0700

    ormclass

--- src/renderer/orm/index.js
+++ src/renderer/orm/index.js
@@ -5,75 +5,76 @@ import { default as definitions } from './definitions'
 import { extend, isEmpty } from 'lodash'
 import associations from './associations'
 import scopes from './scopes'
-
-const Orm = function () {
-  this.options = defaultOptions
-  this.sequelize = null
-  this.models = []
-}
-
-Orm.prototype.withOption = function (options) {
-  this.options = extend({}, defaultOptions, this.options, options)
-  return this
-}
-
-Orm.prototype.defineModels = function () {
-  let sequelize = this.sequelize
-  for (let definition of definitions) {
-    const {tableName, attributes, options} = definition
-    this.models.push(sequelize.define(tableName, attributes, options))
+class Orm {
+  constructor () {
+    this.options = defaultOptions
+    this.sequelize = null
+    this.models = []
   }
-  return this
-}
 
-Orm.prototype.withoutDatabase = function () {
-  this.withOption({ database: null })
-}
+  withOption (options) {
+    this.options = extend({}, defaultOptions, this.options, options)
+    return this
+  }
 
-Orm.prototype.associate = function () {
-  associations(this.sequelize.models)
-  return this
-}
+  defineModels () {
+    let sequelize = this.sequelize
+    for (let definition of definitions) {
+      const {tableName, attributes, options} = definition
+      this.models.push(sequelize.define(tableName, attributes, options))
+    }
+    return this
+  }
 
-Orm.prototype.scope = function () {
-  scopes(this.sequelize.models, this.sequelize)
-  return this
-}
+  withoutDatabase () {
+    this.withOption({ database: null })
+  }
 
-Orm.prototype.recursive = function () {
-  let models = this.list
-  let atur = []
+  associate () {
+    associations(this.sequelize.models)
+    return this
+  }
 
-  const mapper = function (list) {
-    for (let model of list) {
-      let association = model.associations
-      let current = model.tableName
-      if (isEmpty(association)) continue
+  scope () {
+    scopes(this.sequelize.models, this.sequelize)
+    return this
+  }
 
-      for (let item in association) {
-        item = association[item]
-        let type = item.associationType
-        if (type !== 'BelongsTo') {
-          atur.push(item.target)
-          continue
+  recursive () {
+    let models = this.list
+    let atur = []
+
+    const mapper = function (list) {
+      for (let model of list) {
+        let association = model.associations
+        let current = model.tableName
+        if (isEmpty(association)) continue
+
+        for (let item in association) {
+          item = association[item]
+          let type = item.associationType
+          if (type !== 'BelongsTo') {
+            atur.push(item.target)
+            continue
+          }
+          // console.log(association[association_item])
+          const target = item.target.tableName
+          console.log(current + ' BelongsTo ' + target)
         }
-        // console.log(association[association_item])
-        const target = item.target.tableName
-        console.log(current + ' BelongsTo ' + target)
       }
     }
+    mapper(models)
+    // console.log(atur)
   }
-  mapper(models)
-  // console.log(atur)
-}
 
-Orm.prototype.connect = function () {
-  this.sequelize = new Sequelize(this.options)
-  this.defineModels()
-  this.associate()
-  this.scope()
-  this.sequelize.list = this.models
-  return this.sequelize
+  connect () {
+    this.sequelize = new Sequelize(this.options)
+    this.defineModels()
+    this.associate()
+    this.scope()
+    this.sequelize.list = this.models
+    return this.sequelize
+  }
 }
 
 export default Orm
---
+++ src/renderer/orm/index.old.js
@@ -0,0 +1,112 @@
+'use-strict'
+import Sequelize from 'sequelize'
+import { default as defaultOptions } from './options'
+import { default as definitions } from './definitions'
+import { extend, isEmpty, assign } from 'lodash'
+import associations from './associations'
+import scopes from './scopes'
+
+class Torm {
+  // property
+  sequelize = null
+  options = {}
+  models = []
+
+  constructor () {
+    this.options = assign(this.options, defaultOptions)
+    this.sequelize = new Sequelize(this.options)
+    return this
+  }
+
+  defineModels () {
+    let sequelize = this.sequelize
+    for (let definition of definitions) {
+      const {tableName, attributes, options} = definition
+      this.models.push(sequelize.define(tableName, attributes, options))
+    }
+    return this
+  }
+
+  associate () {
+    associations(this.sequelize.models)
+    return this
+  }
+}
+
+const torm = new Torm()
+torm.defineModels()
+torm.associate()
+
+console.log({log: torm.sequelize})
+
+const Orm = function () {
+  this.options = defaultOptions
+  this.sequelize = null
+  this.models = []
+}
+
+Orm.prototype.withOption = function (options) {
+  this.options = extend({}, defaultOptions, this.options, options)
+  return this
+}
+
+Orm.prototype.defineModels = function () {
+  let sequelize = this.sequelize
+  for (let definition of definitions) {
+    const {tableName, attributes, options} = definition
+    this.models.push(sequelize.define(tableName, attributes, options))
+  }
+  return this
+}
+
+Orm.prototype.withoutDatabase = function () {
+  this.withOption({ database: null })
+}
+
+Orm.prototype.associate = function () {
+  associations(this.sequelize.models)
+  return this
+}
+
+Orm.prototype.scope = function () {
+  scopes(this.sequelize.models, this.sequelize)
+  return this
+}
+
+Orm.prototype.recursive = function () {
+  let models = this.list
+  let atur = []
+
+  const mapper = function (list) {
+    for (let model of list) {
+      let association = model.associations
+      let current = model.tableName
+      if (isEmpty(association)) continue
+
+      for (let item in association) {
+        item = association[item]
+        let type = item.associationType
+        if (type !== 'BelongsTo') {
+          atur.push(item.target)
+          continue
+        }
+        // console.log(association[association_item])
+        const target = item.target.tableName
+        console.log(current + ' BelongsTo ' + target)
+      }
+    }
+  }
+  mapper(models)
+  // console.log(atur)
+}
+
+Orm.prototype.connect = function () {
+  this.sequelize = new Sequelize(this.options)
+  this.defineModels()
+  this.associate()
+  this.scope()
+  this.sequelize.list = this.models
+  return this.sequelize
+}
+
+export default Orm
