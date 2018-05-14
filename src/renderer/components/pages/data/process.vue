48cc3ecd51f0c01ff77571b9772a803268dd2fbd
itphcccoid <35017703+idcophccit@users.noreply.github.com>
2018-05-14 14:07:02 +0700

    psg

---
+++ src/renderer/components/pages/data/process.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout>
+    <div>
+      [data]
+    </div>
+  </layout>
+</template>
+
+<script>
+export default {
+  name: 'DataProcess',
+  components: {
+    'layout': () => import('@partials/layout-one')
+  }
+}
+</script>
