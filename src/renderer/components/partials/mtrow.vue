4cae465d1bd23d9deb6212a7ba4ab07871426ddf
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-05-22 08:53:28 +0700

    dep

---
+++ src/renderer/components/partials/mtrow.vue
@@ -0,0 +1,20 @@
+<script>
+import Vue from 'vue'
+const MdTableRow = Vue.component('MdTableRow')
+export default {
+  name: 'Mtrow',
+  extends: MdTableRow,
+  mounted () {
+    console.log(this.$attrs)
+  }/* ,
+  render (createElement) {
+    const options = {
+      attrs: {
+        ...this.attrs
+      }
+    }
+    const row = createElement('md-table-row', options, [this.$slots.default])
+    return row
+  } */
+}
+</script>
