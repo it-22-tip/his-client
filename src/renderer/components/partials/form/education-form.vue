c76e7e4b2b5a59c2661dc67a2ef89790f6c2bb56
itphcccoid <35017703+idcophccit@users.noreply.github.com>
2018-06-28 14:34:02 +0700

    change

--- src/renderer/components/partials/form/education-form.vue
+++ src/renderer/components/partials/form/education-form.vue
@@ -4,8 +4,8 @@
       <p
         v-for="itm in db"
         :key="itm.id">
-        {{ itm.id }}
-        {{ itm.level }}
+        Tingkat: {{ itm.level }}
+        Nama: {{ itm.name }}
         <md-button
           class="md-raised"
           @click="remove(itm)">
@@ -20,24 +20,38 @@
         +
       </md-button>
       <md-field>
-        <label for="movie">Movie</label>
+        <label for="level">Level</label>
         <md-select
-          id="movie"
-          v-model="selected"
-          name="movie">
-          <md-option value="SD">SD</md-option>
-          <md-option value="SMP">SMP</md-option>
-          <md-option value="SMA">SMA</md-option>
-          <md-option value="D3">D3</md-option>
-          <md-option value="S1">S1</md-option>
+          id="level"
+          v-model="current.level"
+          :md-selected="selected"
+          name="level">
+          <md-option
+            v-for="level in availableLevels"
+            :value="level"
+            :key="level">
+            {{ level }}
+          </md-option>
         </md-select>
+      </md-field>
+      <md-field>
+        <label>Provinsi</label>
+        <md-input model="name"/>
+      </md-field>
+      <md-field>
+        <label>Kota</label>
+        <md-input model="name"/>
+      </md-field>
+      <md-field>
+        <label>Nama</label>
+        <md-input model="name"/>
       </md-field>
     </div>
   </div>
 </template>
 
 <script>
-import { filter, random } from 'lodash'
+import { filter, random, find, without } from 'lodash'
 export default {
   name: 'EducationForm',
   data () {
@@ -45,21 +59,57 @@ export default {
       db: [
         {
           id: 1,
-          level: 'SD'
+          level: 'SD',
+          name: 'SD 01 Pagi'
         }
       ],
-      selected: null
+      levels: [
+        'SD',
+        'SMP',
+        'SMA',
+        'D3',
+        'S1',
+        'S2',
+        'S3'
+      ],
+      current: {
+        name: null,
+        level: null,
+        regencyId: null
+      }
     }
+  },
+  computed: {
+    availableLevels: {
+      get () {
+        let sd = find(this.db, function (o) { return o.level === 'SD' })
+        let smp = find(this.db, function (o) { return o.level === 'SMP' })
+        let sma = find(this.db, function (o) { return o.level === 'SMA' })
+        let levels = this.levels
+        if (sd) levels = without(levels, 'SD')
+        if (smp) levels = without(levels, 'SMP')
+        if (sma) levels = without(levels, 'SMA')
+        return levels
+      },
+      set () {
+
+      }
+    }
+  },
+  validation: {
+
   },
   methods: {
     add () {
       let z = [{
         id: random(0, 100),
-        level: this.selected
+        level: this.current.level,
+        name: this.name
       }]
       console.log(z)
       this.db = this.db.concat(z)
       console.log(this.db)
+      this.current.level = null
     },
     remove (itm) {
       console.log('remove ' + itm.id)
@@ -67,6 +117,8 @@ export default {
         return el.id !== itm.id
       })
       this.db = a
+    },
+    selected () {
     }
   }
 }
