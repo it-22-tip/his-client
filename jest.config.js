2e80468bde7721e0f70a350b58bd2b5eea59ef20
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-06-30 12:51:14 +0700

    test

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,33 @@
 module.exports = {
   verbose: true,
   testPathIgnorePatterns: [
-    // '<rootDir>/src/vue-material/'
+    '<rootDir>/src/main/',
+    '<rootDir>/src/renderer/',
+    // '<rootDir>/src/vue-material/',
+    '<rootDir>/external/'
   ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(vue|js)$',
   'moduleFileExtensions': [
     'js',
+    'json',
     'vue'
   ],
   'moduleNameMapper': {
     '^vue$': '<rootDir>/node_modules/vue/dist/vue.common.js',
     '^vue-material(.*)$': '<rootDir>/src/vue-material/dev$1',
-    '^test(.*)$': '<rootDir>/test$1'
-    // '^src(.*)$': '<rootDir>/src$1',
-    // '^theme(.*)$': '<rootDir>/src/theme$1',
-    // '^base(.*)$': '<rootDir>/src/base$1',
-    // '^core(.*)$': '<rootDir>/src/core$1',
-    // '^components(.*)$': '<rootDir>/src/components$1',
-    // '\\.(css|scss)$': '<rootDir>/test/styleMock.js'
+    '^test(.*)$': '<rootDir>/test$1',
+    '^src(.*)$': '<rootDir>/src$1',
+    '^theme(.*)$': '<rootDir>/src/vue-material/dev/theme$1',
+    '^base(.*)$': '<rootDir>/src/vue-material/dev/base$1',
+    '^core(.*)$': '<rootDir>/src/vue-material/dev/core$1',
+    '^components(.*)$': '<rootDir>/src/vue-material/dev/components$1',
+    '\\.(css|scss)$': '<rootDir>/test/styleMock.js'
     // '^@/(.*)$': '<rootDir>/src/renderer/$1'
   },
+  'setupTestFrameworkScriptFile': '<rootDir>/test/setup.js',
   'transform': {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.vue$': '<rootDir>/node_modules/vue-jest'
   },
   'snapshotSerializers': [
     '<rootDir>/node_modules/jest-serializer-vue'
--- src/renderer/components/pages/base/dashboard.test.js
+++ src/renderer/components/pages/base/dashboard.test.js
@@ -1,7 +1,14 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, shallow } from '@vue/test-utils'
+import dashboard from './dashboard.vue'
 import expect from 'expect'
 import psg from './psg'
 
 test('', () => {
   expect(shallowMount(psg).isVueInstance()).toBe(true)
+})
+
+describe('Your Component', () => {
+  it('renders a vue instance', () => {
+    expect(shallow(dashboard)).isVueInstance().toBe(true)
+  })
 })
