42b591e187d4fc9913d5f5d0f0c784d5c057bd8c
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-07-28 06:53:06 +0700

    travis

--- scripts/git-lfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -v
-
-set -e
-
-mkdir -p /tmp/git-lfs && curl -L https://github.com/github/git-lfs/releases/download/v1.2.1/git-lfs-$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux" || echo "darwin")-amd64-1.2.1.tar.gz | tar -xz -C /tmp/git-lfs --strip-components 1 && /tmp/git-lfs/git-lfs pull
--- scripts/travis/rvm-stable.sh
+++ scripts/travis/rvm-stable.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -v
-
 set -e
-
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer
 curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
